a986f9eed4d9ede5fa609a4a4ab80942ee4b4cc5
eopsd <51473248@qq.com>
2015-11-01 22:36:15 +0800

    front page

--- eospd_h5/src/com/eospd/modules/Collectindexday.java
+++ eospd_h5/src/com/eospd/modules/Collectindexday.java
@@ -4,9 +4,9 @@ import org.nutz.mvc.annotation.At;
 import org.nutz.mvc.annotation.Filters;
 import org.nutz.mvc.annotation.Ok;
 
-public class Meter {
+public class Collectindexday {
 	
-	@At("/meter")
+	@At("/")
 	@Ok("jsp:jsp.index")
 	@Filters // 覆盖UserModule类的@Filter设置,因为登陆可不能要求是个已经登陆的Session
     public String doHello() {
